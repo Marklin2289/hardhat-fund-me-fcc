0c4831790b956a42c8b4f91d1887329bf9f4a122
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-06-26 22:04:22 -0700

    setup mocks for localhost and run yarn hardhat deploy --tags mocks

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -9,7 +9,13 @@ const networkConfig = {
     },
     // 31337 ? localhost network
 }
+const developmentChains = ["hardhat", "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 200000000000
 
 module.exports = {
     networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
 }
