aa7fef7b97a4ae7bbe439e8a160bef2cfd3822c4
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-06-29 17:54:55 -0700

    tested withdrew from single funder

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -43,4 +43,37 @@ describe("FundMe", async function () {
             assert.equal(response, deployer)
         })
     })
+    describe("withdraw", async function () {
+        // adding beforeEach() to fund the account first
+        beforeEach(async function () {
+            await fundMe.fund({ value: sendValue })
+        })
+        it("Withdraw ETH from a single funder", async () => {
+            // Arrange
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            // Act
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            // Assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+        })
+    })
 })
